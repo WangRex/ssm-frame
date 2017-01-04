762dd601f9ea56cbdd908fb40c005ef04c2fbd2b
WangRex <503431920@qq.com>
2017-01-04 18:18:30 +0800

    Spring SpringMVC Mybatis frame

---
+++ ssm-frame-bean/src/main/java/org/ssm/frame/bean/Constant.java
@@ -0,0 +1,23 @@
+/**   
+* @Title: Constant.java    
+* @Package org.ssm.frame.bean    
+* @Description: TODO(用一句话描述该文件做什么)    
+* @author rex   
+* @date Jan 4, 2017 6:15:21 PM    
+* @version V1.0   
+*/
+package org.ssm.frame.bean;
+
+/**   
+* @ClassName: Constant    
+* @Description: TODO(这里用一句话描述这个类的作用)    
+* @author rex    
+* @date Jan 4, 2017 6:15:21 PM    
+*         
+*/
+public class Constant {
+	
+	/** 逗号 */
+	public static final String COMMA = ",";
+
+}
